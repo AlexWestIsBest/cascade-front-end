f6d6863ad2c121f01c9f8086b6686bf99398a3d2
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-16 14:56:02 -0500

    Corrects copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = (props) => {
     return (
         <div className="footer">
-            <span>℗ Cascade Social Media</span>
+            <span>© Cascade Social Media</span>
         </div>
     );
 }
