9373c3b3303b51c299a9d16d2f0ca1a565abf7c8
Patrick Clary <patrickm@theclarys.org>
2023-01-16 14:54:45 -0600

    Pull Posts data into Main state. Pass to Newsfeed.

--- src/pages/Newsfeed.js
+++ src/pages/Newsfeed.js
@@ -1,8 +1,24 @@
-import React from "react";
+import  { useState } from "react";
 
-const Newsfeed = () => {
+const Newsfeed = (props) => {
+
+    const loaded = () => {
+        return props.posts.map((post) => (
+            <div key={post._id} className="post">
+                <h3>{post.title}</h3>
+                <p><img className="postImg" src={post.image} alt={post.title} /></p>
+                <p>{post.content}</p>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <h3>Loading...</h3>;
+    };
+
     return <div>
             <h2>Newsfeed Page</h2>
+            {props.posts ? loaded() : loading()}
         </div>;
     };
 
