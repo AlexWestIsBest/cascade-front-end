177df1a3516b13f179c18e9ad1af541b07ee5d91
Patrick Clary <patrickm@theclarys.org>
2023-01-16 10:35:03 -0600

    Add pages via Routes to Main. Add Links to Nav.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Newsfeed from "../pages/Newsfeed";
 
 const Main = (props) => {
     return (
         <div className="main">
-            <h1>Main</h1>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/newsfeed" element={<Newsfeed />} />
+            </Routes>
         </div>
     );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Navigation = (props) => {
     return (
         <div className="nav">
-            <h1>Navigation</h1>
+            <Link to="/">
+                <div>Home</div>
+            </Link>
+            <Link to="/newsfeed">
+                <div>Newsfeed</div>
+            </Link>
+            <Link to="/about">
+                <div>About</div>
+            </Link>
         </div>
     );
 }
