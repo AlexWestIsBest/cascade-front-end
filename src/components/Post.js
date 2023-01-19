1d133bed86110ae575626ed2a536ca87b51cf8f3
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-18 23:13:55 -0500

    Adds editable title text field

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,20 @@
 import ActionBar from "./ActionBar";
+import { useState } from "react";
+import EditableText from "./EditableText";
 
 const Post = (props) => {
+    const [value, setValue] = useState(props.post.title)
+
+    const updatePost = async (id, updatedPost) => {
+        await fetch(props.URL + id, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedPost)
+        })
+    }
+
     const deletePost = async (id) => {
         await fetch(props.URL + id, {
             method: 'DELETE'
@@ -10,7 +24,7 @@ const Post = (props) => {
 
     return (
         <>
-            <h3>{props.post.title}</h3>
+            <EditableText id={props.id} value={value} setValue={setValue} updatePost={updatePost} URL={props.URL} />
             <p><img className="postImg" src={props.post.image} alt={props.post.title} /></p>
             <p>{props.post.content}</p>
             <ActionBar id={props.id} URL={props.URL} deletePost={deletePost} /> 
