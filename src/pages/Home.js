c0ef784120e847758c33df92b2096178a643a841
Patrick Clary <patrickm@theclarys.org>
2023-01-20 18:12:42 -0600

    Add loaded : loading function to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,81 +38,93 @@ const Home = ({ user }) => {
     }
 
     // loaded state
-    let userName = null
-    if (!currentUser.displayName || currentUser.displayName === "No User Info") {
-        // currentUser.displayName = "No User Info"
-        userName = currentUser.email
-    } else {
-        userName = currentUser.displayName.split(" ")[0];
+    const loaded = () => {
+        let userName = null
+        if (!currentUser.displayName || currentUser.displayName === "No User Info") {
+            // currentUser.displayName = "No User Info"
+            userName = currentUser.email
+        } else {
+            userName = currentUser.displayName.split(" ")[0];
+        }
+
+        return (
+            <section>
+            <div className='homePage'>
+                <h4>
+                    Hello {userName}!
+                </h4>
+                <form className="updateForm" >
+                        <span>
+                            UID: {profile.uid}
+                        </span>
+                        <span>
+                            <label htmlFor="email">Email: </label>
+                            <input 
+                                type="text" 
+                                name="email" 
+                                value={profile.email}
+                                // onChange={handleChange}
+                            />
+                        </span>
+                        <span>
+                            <label htmlFor="displayName">Full Name: </label>
+                            <input 
+                                type="text" 
+                                name="displayName"
+                                value={profile.displayName}
+                                // onChange={handleChange}
+                            />
+                        </span>
+                        <button
+                            type="button"
+                            className='btn btn-sm btn-success'
+                            // onClick={updateProfile}
+                            ><span className="bi bi-archive"></span>&nbsp;Update Profile
+                        </button>
+                </form>
+                <form className="updateForm" >
+                        <span>
+                            <label htmlFor="newPassword">New Password: </label>
+                            <input 
+                                type="password" 
+                                name="newPassword" 
+                                value={newPassword}
+                                // onChange={handleNewPassword} 
+                            />
+                        </span>
+                        <button
+                            type="button"
+                            className='btn btn-sm btn-success'
+                            // onClick={updatePassword}
+                            ><span className="bi bi-arrow-clockwise"></span>&nbsp;Set Password
+                        </button>
+                </form>
+                <div className="delDiv">
+                    <h5>Warning: Clicking here will delete your account!</h5>
+                    <button
+                        type="button"
+                        className='btn btn-sm btn-danger' 
+                        // onClick={delAccount}
+                        ><span className="bi-trash"></span>&nbsp;Delete Account
+                    </button>
+                </div>
+                <p>Post history coming soon...</p>
+            </div>
+            
+            </section>
+        )
+    }
+
+    const loading = () => {
+        return <h3>Loading...</h3>
     }
 
     return (
         <section>
-        <div className='homePage'>
-            <h4>
-                Hello {userName}!
-            </h4>
-            <form className="updateForm" >
-                    <span>
-                        UID: {profile.uid}
-                    </span>
-                    <span>
-                        <label htmlFor="email">Email: </label>
-                        <input 
-                            type="text" 
-                            name="email" 
-                            value={profile.email}
-                            // onChange={handleChange}
-                        />
-                    </span>
-                    <span>
-                        <label htmlFor="displayName">Full Name: </label>
-                        <input 
-                            type="text" 
-                            name="displayName"
-                            value={profile.displayName}
-                            // onChange={handleChange}
-                        />
-                    </span>
-                    <button
-                        type="button"
-                        className='btn btn-sm btn-success'
-                        // onClick={updateProfile}
-                        ><span className="bi bi-archive"></span>&nbsp;Update Profile
-                    </button>
-            </form>
-            <form className="updateForm" >
-                    <span>
-                        <label htmlFor="newPassword">New Password: </label>
-                        <input 
-                            type="password" 
-                            name="newPassword" 
-                            value={newPassword}
-                            // onChange={handleNewPassword} 
-                        />
-                    </span>
-                    <button
-                        type="button"
-                        className='btn btn-sm btn-success'
-                        // onClick={updatePassword}
-                        ><span className="bi bi-arrow-clockwise"></span>&nbsp;Set Password
-                    </button>
-            </form>
-            <div className="delDiv">
-                <h5>Warning: Clicking here will delete your account!</h5>
-                <button
-                    type="button"
-                    className='btn btn-sm btn-danger' 
-                    // onClick={delAccount}
-                    ><span className="bi-trash"></span>&nbsp;Delete Account
-                </button>
-            </div>
-            <p>Post history coming soon...</p>
-        </div>
-        
+            {user ? loaded() : loading()}
         </section>
     )
 
-    };
+};
 
 export default Home;
