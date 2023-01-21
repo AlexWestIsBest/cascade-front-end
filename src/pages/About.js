28424dfb9d3407a75a5b42907811fbc39089b947
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-21 15:00:42 -0500

    Adds more detail

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const About = () => {
     return <div>
-            <p>Thank you for using Cascade, a social media
- site built using the MERN stack. This site was built as a collaboration between Alex West, Patrick Clary, and Victoria Onyema as a mid term project during a General Assembly Software Immersive bootcamp.</p>  
+            <p>Thank you for using Cascade, a social media site built using the MERN stack.</p>
+            <p>This site was built as a collaboration between Alex Westerlund, Patrick Clary, and Victoria Onyema as a mid-term project of the General Assembly Software Immersive bootcamp.</p>  
+            <p>We hope you enjoy our work. Please feel free to add a post of your own!</p>
        </div>;
     };
 
